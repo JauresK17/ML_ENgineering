1f423adb5084fc31a1d7de2ec410808db7b1194e
JauresK17 <kouamelaffind@outlokk.fr>
2026-09-12 13:15:11 +0000

    prediction-pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,60 +1,62 @@
 import os
-import pickle
 import sys
 
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exceptions import CustomException
 
-
-def save_object(file_path: str, obj) -> None:
-    """Serialise un objet Python sur disque."""
+def save_object(file_path, obj):
     try:
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-
-
-def load_object(file_path: str):
-    """Recharge un objet serialise par save_object."""
-    try:
-        with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
-    except Exception as e:
-        raise CustomException(e, sys)
-
-
-def evaluate_models(X_train, y_train, X_test, y_test, models: dict, params: dict) -> dict:
-    """
-    Entraine chaque modele avec recherche d'hyperparametres.
-
-    Renvoie un dict : nom -> {"train_r2", "test_r2", "best_params", "model"}.
-    Le modele renvoye est celui reellement entraine, ce qui evite d'aller
-    le rechercher dans le dictionnaire d'origine.
-    """
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for nom, model in models.items():
-            grille = params.get(nom, {})
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            search = GridSearchCV(model, grille, cv=3)
-            search.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            model.set_params(**search.best_params_)
-            model.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            report[nom] = {
-                "train_r2": r2_score(y_train, model.predict(X_train)),
-                "test_r2": r2_score(y_test, model.predict(X_test)),
-                "best_params": search.best_params_,
-                "model": model,
-            }
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
